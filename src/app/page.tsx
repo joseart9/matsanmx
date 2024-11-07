6a273fb701eced57996402f4b64f52bc30bbc058
Arturo <A01177145@tec.mx>
2024-11-07 16:54:00 -0600

    bettter desktop view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import ProductCard from "@/app/components/ProductCard";
 import Product from "@/types/Product";
 import NovedadesCaroussel from "@/app/components/NovedadesCaroussel";
@@ -8,8 +7,6 @@ import { useProductos } from "@/hooks/useProductos";
 import { CircularProgress } from "@nextui-org/react";
 import EmptyProductsSVG from "./components/EmptyProductsSVG";
 import { useNovedades } from "@/hooks/useNovedades";
-import { use, useEffect, useState } from "react";
-import { Novedad } from "@/types/Novedades";
 
 export default function Home() {
   const { productos, loading, error } = useProductos();
@@ -22,15 +19,23 @@ export default function Home() {
   );
 
   return (
-    <main className="flex min-h-screen w-screen flex-col items-center bg-primary">
+    <main className="flex min-h-screen min-w-screen flex-col items-center bg-primary">
       <Navbar />
-      <section className="flex w-full h-full flex-col p-2 bg-primary mt-20">
-        <section className="flex flex-col w-full h-full">
+      <section className="flex w-full h-fit p-1 bg-accent mt-16 items-center justify-between">
+        <p className="text-primary text-xs ml-5 uppercase">
+          ✈️ Envíos nacionales y locales
+        </p>
+        <a href="https://www.instagram.com/matsanmx/">
+          <img src="/instagram.svg" alt="Argentina" className="text-primary fill-primary w-5 h-5 mr-5" />
+        </a>
+      </section>
+      <section className="flex w-full h-full flex-col p-2 bg-primary mt-2 lg:mt-4">
+        <section className="flex flex-col w-full h-full lg:pr-20 lg:pl-20 lg:pb-0">
           <h1 className="text-2xl text-center font-light p-1 text-accent tracking-wide mb-5">NUEVA COLECCI&Oacute;N</h1>
           <NovedadesCaroussel productsTendencia={novedades} />
         </section>
-        <section className="flex flex-col w-full h-full mt-5">
-          <div className="gap-2 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 w-full">
+        <section className="flex flex-col w-full h-full mt-5 lg:p-20 lg:pt-0">
+          <div className="gap-2 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 lg:gap-6 w-full">
             {productos.map((product, index) => (
               <ProductCard key={index} product={product as Product} />
             ))}
@@ -43,7 +48,11 @@ export default function Home() {
           )}
         </section>
       </section>
-
+      <footer>
+        <div className="flex w-full h-20 items-center justify-center">
+          <a href="https://www.instagram.com/araf.innovations/" className="text-secondary">Created by Araf Innovations 2024</a>
+        </div>
+      </footer>
     </main>
   )
 }
